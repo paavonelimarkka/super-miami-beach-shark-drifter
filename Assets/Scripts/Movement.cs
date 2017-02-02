dae797045df950f411e2b7b01a0fd9e74288d0d1
paavonelimarkka <paavo.nelimarkka@gmail.com>
2017-02-02 13:46:54 +0200

    fix scoremanager

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,6 +14,7 @@ public class Movement : MonoBehaviour {
 
 	void Start() {
 		carRB = GetComponent<Rigidbody>();
+        SetEndScore.GameOver = false;
 	}
 
 	void Update () 
--- Assets/Scripts/SetEndScore.cs
+++ Assets/Scripts/SetEndScore.cs
@@ -11,20 +11,16 @@ public class SetEndScore : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameOver = false;
 		loppuSaatanaPisteet = UpdatePoints.points;
 	}
 
 	// Update is called once per frame
 	void Update () {
+        Debug.Log(GameOver);
         if (GameOver == false)
         {
             loppuSaatanaPisteet = UpdatePoints.points;
             kala.text = loppuSaatanaPisteet.ToString();
         }
-		if (GameOver == true) {
-			kala.text = loppuSaatanaPisteet.ToString();
-		}
-		
 	}
 }
